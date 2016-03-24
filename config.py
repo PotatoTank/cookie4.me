08525b81c82ff573f45d2803479b41aa64a3fb21
Joey <joeyli96@hotmail.com>
2016-03-24 14:42:23 -0700

    Postgres

--- config.py
+++ config.py
@@ -9,6 +9,20 @@ OPENID_PROVIDERS = [
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
 
 import os
+import psycopg2
+import urlparse
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = postgresql://ximvvccibohdap:gmkJT0lAppHwzD_Fe0jNf6dWmO@ec2-107-20-224-236.compute-1.amazonaws.com/d34k3eiptp679u
