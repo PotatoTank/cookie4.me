341e012c85167ce2a60abf8ccd48b129a18a6782
Joey <joeyli96@hotmail.com>
2016-03-24 15:01:10 -0700

    fixed python version

--- config.py
+++ config.py
@@ -9,22 +9,14 @@ OPENID_PROVIDERS = [
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
 
 import os
-import psycopg2
-import urlparse
-
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
-
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = postgresql://ximvvccibohdap:gmkJT0lAppHwzD_Fe0jNf6dWmO@ec2-107-20-224-236.compute-1.amazonaws.com/d34k3eiptp679u
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(basedir, 'app.db') +
+                               '?check_same_thread=False')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_RECORD_QUERIES = True
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
